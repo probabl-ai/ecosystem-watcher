2689340017b7a03f9d8b6c2b6ab7fa5794f4f464
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-02-06 16:25:36 +0000

    fixed github link

--- scripts/render.py
+++ scripts/render.py
@@ -8,10 +8,13 @@ template_path = Path(__file__).parent.parent / "docs" / "template.html"
 template = Template(template_path.read_text())
 data_path = Path(__file__).parent.parent / "docs" / "data.jsonl"
 items = list(srsly.read_jsonl(data_path))
+items = sorted(items, key=lambda d: -d['month_downloads'])
 for item in items:
     print(f"Generating stats for {item['full_name']=}")
     item['age'] = humanize.naturaldelta(dt.datetime.now() - dt.datetime.fromisoformat(item['created_at'][:10]))
+    item['age_days'] = (dt.datetime.now() - dt.datetime.fromisoformat(item['created_at'][:10])).days
     item['since_update'] = humanize.naturaldelta(dt.datetime.now() - dt.datetime.fromisoformat(item['pushed_at'][:10]))
+    item['since_update_days'] = (dt.datetime.now() - dt.datetime.fromisoformat(item['pushed_at'][:10])).days
     item["total_downloads"] = humanize.intcomma(item["total_downloads"])
     item["month_downloads"] = humanize.intcomma(item["month_downloads"])
 
