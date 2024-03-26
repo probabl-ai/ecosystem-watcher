e83a2b14057dcf8ab3a1cf26f8bcff7b19e9c744
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-03-26 12:02:32 +0000

    new provider of data

--- scripts/corese.py
+++ scripts/corese.py
@@ -1,13 +1,13 @@
 import srsly 
 import datetime as dt 
 import requests as rq
-from tenacity import retry
+from stamina import retry
 
 today = str(dt.datetime.now())[:10]
 
 # Handle all Github data
 
-@retry
+@retry(on=Exception,attempts=5)
 def fetch_github_stats():
     resp_releases = rq.get("https://api.github.com/repos/Wimmics/corese/releases")
     data = []
@@ -20,17 +20,21 @@ def fetch_github_stats():
     srsly.write_jsonl(f"data/corese/github-{today}.jsonl", data)
 
 
-@retry
+def _arches_downloads(entry):
+    return sum(entry['arches']['x86_64'])
+
+@retry(on=Exception, attempts=5)
 def fetch_flathub_stats():
+    print("fetching flathub stats")
     data = []
     for name in ['CoreseCommand', 'CoreseGui']:
-        resp_flathub = rq.get(f"https://flathub.org/_next/data/60a2786bb9d4c2b2b30a218033e961157168c0a4/en/apps/fr.inria.corese.{name}.json?appDetails=fr.inria.corese.CoreseGui")
-        downloads = resp_flathub.json()['pageProps']['stats']
+        resp_flathub = rq.get(f"https://klausenbusk.github.io/flathub-stats/data/fr.inria.corese.{name}.json")
+        downloads = resp_flathub.json()['stats']
         data.append({
             'today': today,
-            'installs_total': downloads['installs_total'],
-            'installs_last_month': downloads['installs_last_month'],
-            'installs_last_7_days': downloads['installs_last_7_days'],
+            'installs_total': None,
+            'installs_last_month': sum([_arches_downloads(e) for e in downloads[-30:]]),
+            'installs_last_7_days': sum([_arches_downloads(e) for e in downloads[-7:]]),
             'name': name,
         })
     srsly.write_jsonl(f"data/corese/flathub-{today}.jsonl", data)
