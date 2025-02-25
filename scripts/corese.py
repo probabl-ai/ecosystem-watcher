e6f794d42057462893b00f06aa84a8e6c63334da
Jerome Schneider <jerome@probabl.ai>
2025-02-25 12:30:12 +0100

    Fix Corese script when arch is not x86_64

--- scripts/corese.py
+++ scripts/corese.py
@@ -1,42 +1,52 @@
-import srsly 
-import datetime as dt 
+import srsly
+import datetime as dt
 import requests as rq
-from stamina import retry
 
 today = str(dt.datetime.now())[:10]
 
 # Handle all Github data
 
-@retry(on=Exception,attempts=5)
+
 def fetch_github_stats():
     resp_releases = rq.get("https://api.github.com/repos/Wimmics/corese/releases")
     data = []
     for release in resp_releases.json():
-        for asset in release['assets']:
-            d = {k: asset[k] for k in ['download_count', 'name']}
-            d['today'] = today
+        for asset in release["assets"]:
+            d = {k: asset[k] for k in ["download_count", "name"]}
+            d["today"] = today
             data.append(d)
 
     srsly.write_jsonl(f"data/corese/github-{today}.jsonl", data)
 
 
 def _arches_downloads(entry):
-    return sum(entry['arches']['x86_64'])
+    total = 0
+    for arch in entry["arches"]:
+        total += sum(entry["arches"][arch])
+    return total
 
-@retry(on=Exception, attempts=5)
+
 def fetch_flathub_stats():
     print("fetching flathub stats")
     data = []
-    for name in ['CoreseCommand', 'CoreseGui']:
-        resp_flathub = rq.get(f"https://klausenbusk.github.io/flathub-stats/data/fr.inria.corese.{name}.json")
-        downloads = resp_flathub.json()['stats']
-        data.append({
-            'today': today,
-            'installs_total': None,
-            'installs_last_month': sum([_arches_downloads(e) for e in downloads[-30:]]),
-            'installs_last_7_days': sum([_arches_downloads(e) for e in downloads[-7:]]),
-            'name': name,
-        })
+    for name in ["CoreseCommand", "CoreseGui"]:
+        resp_flathub = rq.get(
+            f"https://klausenbusk.github.io/flathub-stats/data/fr.inria.corese.{name}.json"
+        )
+        downloads = resp_flathub.json()["stats"]
+        data.append(
+            {
+                "today": today,
+                "installs_total": None,
+                "installs_last_month": sum(
+                    [_arches_downloads(e) for e in downloads[-30:]]
+                ),
+                "installs_last_7_days": sum(
+                    [_arches_downloads(e) for e in downloads[-7:]]
+                ),
+                "name": name,
+            }
+        )
     srsly.write_jsonl(f"data/corese/flathub-{today}.jsonl", data)
 
 
